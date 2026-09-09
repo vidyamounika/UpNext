cd6c3adcf3551a1ac2d448a81435a9298631f0a4
vidya <vidya.jinka@accenture.com>
2026-09-09 23:12:20 +0530

    Added BE url

--- backend/main.py
+++ backend/main.py
@@ -594,7 +594,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://upnext-api-m7ix.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
